dc5da4a684aec65bc61f2db877ed78261122b3f9
jorge.reyes <jorge.reyes@globant.com>
2014-09-10 16:51:08 -0500

    Forecast Adapter added to show more information for list and detail elements

--- app/src/main/java/com/example/android/sunshine/app/Utility.java
+++ app/src/main/java/com/example/android/sunshine/app/Utility.java
@@ -7,6 +7,9 @@ import android.preference.PreferenceManager;
 import com.example.android.sunshine.app.data.WeatherContract;
 
 import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -14,17 +17,12 @@ import java.util.Date;
  */
 public class Utility {
 
+    public static final String DATE_FORMAT = "yyyyMMdd";
+
     public static String getPreferredLocation(Context context) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         return prefs.getString(context.getString(R.string.pref_location_key),
                 context.getString(R.string.pref_location_default));
-    }
-
-    public static boolean isMetric(Context context) {
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
-        return prefs.getString(context.getString(R.string.pref_units_key),
-                context.getString(R.string.pref_units_metric))
-                .equals(context.getString(R.string.pref_units_metric));
     }
 
     static String formatTemperature(double temperature, boolean isMetric) {
@@ -40,5 +38,98 @@ public class Utility {
     static String formatDate(String dateString) {
         Date date = WeatherContract.getDateFromDb(dateString);
         return DateFormat.getDateInstance().format(date);
+    }
+
+    static String getFriendlyDayString(Context context, String dateString){
+
+        Date todayDate = new Date();
+        String todayStr = WeatherContract.getDbStringDate(todayDate);
+        Date inputDate = WeatherContract.getDateFromDb(dateString);
+
+        // If the date we're building the String for is today's date, the format
+        // is "Today, June 24"
+        if (todayStr.equals(dateString)) {
+            String today = context.getString(R.string.today);
+            return context.getString(
+                    R.string.format_full_friendly_date,
+                    today,
+                    getFormattedMonthDay(context, dateString));
+        } else {
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(todayDate);
+            cal.add(Calendar.DATE, 7);
+            String weekFutureString = WeatherContract.getDbStringDate(cal.getTime());
+
+            if (dateString.compareTo(weekFutureString) < 0) {
+                // If the input date is less than a week in the future, just return the day name.
+                return getDayName(context, dateString);
+            } else {
+                // Otherwise, use the form "Mon Jun 3"
+                SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
+                return shortenedDateFormat.format(inputDate);
+            }
+        }
+    }
+
+    public static String getDayName(Context context, String dateStr) {
+        SimpleDateFormat dbDateFormat = new SimpleDateFormat(Utility.DATE_FORMAT);
+        try {
+            Date inputDate = dbDateFormat.parse(dateStr);
+            Date todayDate = new Date();
+            // If the date is today, return the localized version of "Today" instead of the actual
+            // day name.
+            if (WeatherContract.getDbStringDate(todayDate).equals(dateStr)) {
+                return context.getString(R.string.today);
+            } else {
+                // If the date is set for tomorrow, the format is "Tomorrow".
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(todayDate);
+                cal.add(Calendar.DATE, 1);
+                Date tomorrowDate = cal.getTime();
+                if (WeatherContract.getDbStringDate(tomorrowDate).equals(
+                        dateStr)) {
+                    return context.getString(R.string.tomorrow);
+                } else {
+                    // Otherwise, the format is just the day of the week (e.g "Wednesday".
+                    SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE");
+                    return dayFormat.format(inputDate);
+                }
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+            // It couldn't process the date correctly.
+            return "";
+        }
+    }
+
+    /**
+     * Converts db date format to the format "Month day", e.g "June 24".
+     * @param context Context to use for resource localization
+     * @param dateStr The db formatted date string, expected to be of the form specified
+     *                in Utility.DATE_FORMAT
+     * @return The day in the form of a string formatted "December 6"
+     */
+    public static String getFormattedMonthDay(Context context, String dateStr) {
+        SimpleDateFormat dbDateFormat = new SimpleDateFormat(Utility.DATE_FORMAT);
+        try {
+            Date inputDate = dbDateFormat.parse(dateStr);
+            SimpleDateFormat monthDayFormat = new SimpleDateFormat("MMMM dd");
+            String monthDayString = monthDayFormat.format(inputDate);
+            return monthDayString;
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Returns true if metric unit should be used, or false if
+     * imperial units should be used.
+     */
+    public static boolean isMetric(Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        return prefs.getString(context.getString(R.string.pref_units_key),
+                context.getString(R.string.pref_units_metric)).equals(
+                context.getString(R.string.pref_units_metric));
     }
 }
